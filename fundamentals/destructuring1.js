316c02bb3fcc2885d0a136fb2d998fc39b644007
Marcos Vyctor <115595096+marcosvgalupo@users.noreply.github.com>
2023-05-30 22:55:06 -0300

    Update destructuring1.js

--- fundamentals/destructuring1.js
+++ fundamentals/destructuring1.js
@@ -14,7 +14,7 @@ console.log(name, age)
 const {name: n, age: a} = person
 console.log(n, a)
 
-const {last_name, defaultBoolean = true} = person // if defaultBoolean is undefined, by default, yout value is set to true
+const {last_name, defaultBoolean = true} = person // if defaultBoolean is undefined, by default, your value is set to true
 console.log(last_name, defaultBoolean)
 
 const { adress: {street: s, number: nb}} = person
